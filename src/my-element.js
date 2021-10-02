18ce6dd56043f59131c83d3b920fd2ca5d0ed3ad
Remco Gubbels <remco090@gmail.com>
2021-10-02 12:51:10 +0200

    init: initial setup

---
+++ src/my-element.js
@@ -0,0 +1,78 @@
+import { html, css, LitElement } from 'lit'
+import * as THREE from 'three';
+
+/**
+ * An example element.
+ *
+ * @slot - This element has a slot
+ * @csspart button - The button
+ */
+export class MyElement extends LitElement {
+  static get styles() {
+    return css`
+      :host {
+        display: block;
+        border: solid 1px gray;
+        padding: 16px;
+        max-width: 800px;
+      }
+    `
+  }
+
+  static get properties() {
+    return {
+      /**
+       * The name to say "Hello" to.
+       */
+      name: { type: String },
+
+      /**
+       * The number of times the button has been clicked.
+       */
+      count: { type: Number }
+    }
+  }
+
+  connectedCallback() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+
+    const geometry = new THREE.BoxGeometry();
+    const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    const cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+
+    camera.position.z = 5;
+
+    const animate = function () {
+      requestAnimationFrame( animate );
+  
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+  
+      renderer.render( scene, camera );
+    };
+
+    animate();
+  }
+
+  render() {
+    return html`
+      <h1>Hello, ${this.name}!</h1>
+      <button @click=${this._onClick} part="button">
+        Click Count: ${this.count}
+      </button>
+      <slot></slot>
+    `
+  }
+
+  _onClick() {
+    this.count++
+  }
+}
+
+window.customElements.define('my-element', MyElement)
